10151700143a1a3d935dc0bf94e25a012846c34e
egjallo <egjallow10@gmail.com>
2022-11-13 15:31:13 +0000

    edited

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,12 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            #self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = self.hash_case_insensitive(pwd)
+
+    @staticmethod
+    def hash_case_insensitive(pwd):
+        return hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +59,8 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        #return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return self.hash_case_insensitive(pwd) == self.password
 
 
 if __name__ == '__main__':
